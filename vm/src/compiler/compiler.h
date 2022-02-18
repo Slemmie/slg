82c7b446323e237b73818503c694295becfe1495
Slemmie <thorvejeneriksen@gmail.com>
2022-02-18 20:10:30 +0100

    create compile function signature

--- vm/src/compiler/compiler.h
+++ vm/src/compiler/compiler.h
@@ -6,6 +6,9 @@
 //   a list of resolved symbols with their implementations
 // implementations are a sequence of instructions, structured much like assembly
 
-//     //
-// ... //
-//     //
+// for now, we do not support flags/debug-modes/etc.
+
+#pragma once
+
+// returns 0 if nothing went wrong
+extern int compile(const char* output_file, char char* input_file);

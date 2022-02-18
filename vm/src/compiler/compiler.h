2977001e7c6b0be16316962e77d8c7dca9943c9c
Slemmie <thorvejeneriksen@gmail.com>
2022-02-18 20:07:26 +0100

    general idea of compilation module written with comment

--- vm/src/compiler/compiler.h
+++ vm/src/compiler/compiler.h
@@ -1,5 +1,11 @@
 // "entry point" of the compiler module
 
+// outputs a .o file, takes exactly one source code file
+// .o file contains:
+//   a list of unresolved symbols
+//   a list of resolved symbols with their implementations
+// implementations are a sequence of instructions, structured much like assembly
+
 //     //
 // ... //
 //     //

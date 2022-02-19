7f2e332049b493dc1afab3bd9ee453e995ad51f4
Slemmie <thorvejeneriksen@gmail.com>
2022-02-20 00:53:10 +0100

    add reading of input_file in compile() + add compiler.c to build

--- vm/src/compiler/compiler.c
+++ vm/src/compiler/compiler.c
@@ -1,5 +1,12 @@
 // implementation of int compile(const char*, const char*)
 
+#include "../util/read_file.h"
+
+#include <stdio.h>
+
 int compile(const char* output_file, const char* input_file) {
+	// aquire source code from input_file
+	char* source_code = read_source(input_file);
 	
+	return 0;
 }

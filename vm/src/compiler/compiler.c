245435ceb9dcfc28cff83e44221cc9bedb1a4c11
Slemmie <thorvejeneriksen@gmail.com>
2022-02-20 01:00:54 +0100

    add error check after reading source code

--- vm/src/compiler/compiler.c
+++ vm/src/compiler/compiler.c
@@ -1,12 +1,19 @@
 // implementation of int compile(const char*, const char*)
 
 #include "../util/read_file.h"
+#include "../util/exit_codes.h"
 
 #include <stdio.h>
 
 int compile(const char* output_file, const char* input_file) {
 	// aquire source code from input_file
 	char* source_code = read_source(input_file);
+	
+	// check if some error occoured while reading source code from input_file
+	if (!source_code) {
+		printf("[fatal error]: failed to read input file '%s'\n", input_file);
+		exit(SLG_EXIT_FILE_ERROR);
+	}
 	
 	return 0;
 }

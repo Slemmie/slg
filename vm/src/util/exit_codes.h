e41a31595ff3dbb88be432e082385ce6b3dba586
Slemmie <thorvejeneriksen@gmail.com>
2022-02-26 22:10:44 +0100

    add source_code_error exit code

--- vm/src/util/exit_codes.h
+++ vm/src/util/exit_codes.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
-#define SLG_EXIT_SUCCESSFUL (0 << 2)
-#define SLG_EXIT_BAD_ARGV   (1 << 2)
-#define SLG_EXIT_FILE_ERROR (2 << 2)
+#define SLG_EXIT_SUCCESSFUL        (0 << 2)
+#define SLG_EXIT_BAD_ARGV          (1 << 2)
+#define SLG_EXIT_FILE_ERROR        (2 << 2)
+#define SLG_EXIT_SOURCE_CODE_ERROR (3 << 2)

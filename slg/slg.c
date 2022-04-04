8a4bcce26ab28eb9a96b4dadfb3bd4ffdfd3f60c
Slemmie <thorvejeneriksen@gmail.com>
2022-04-05 01:29:27 +0200

    entry point + layout plan

---
+++ slg/slg.c
@@ -0,0 +1,17 @@
+// slg entry point
+
+int main(int argc, char** argv) {
+	// parse command line arguments
+	
+	// configure tmp directory
+	
+	// sort out input file(s) and output file(s)
+	
+	// perform compilation
+	
+	// perform assembling
+	
+	// perform linking
+	
+	return 0;
+}

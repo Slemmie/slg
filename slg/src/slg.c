fd45406288aa97f1e77bee94cb384c1899d958be
Slemmie <thorvejeneriksen@gmail.com>
2022-04-06 01:04:13 +0200

    tweak comments in slg.c

--- slg/src/slg.c
+++ slg/src/slg.c
@@ -4,23 +4,20 @@
 #include "tmp_dir_manager.h"
 
 int main(int argc, char** argv) {
-	// parse command line arguments
+	// 1. parse command line arguments
 	Argv_info argv_info;
 	parse_argv(&argv_info, argc, argv);
 	
-	// configure tmp directory
+	// 2. configure tmp directory
 	create_tmp_dir();
 	
-	// sort out input file(s) and output file
+	// 3. perform compilation
 	
-	// perform compilation
+	// 4. perform assembling
 	
-	// perform assembling
+	// 5. perform linking
 	
-	// perform linking
-	
-	// clean up
+	// 6. clean up
 	destruct_argv_info(&argv_info);
-	
 	clean_exit(0);
 }

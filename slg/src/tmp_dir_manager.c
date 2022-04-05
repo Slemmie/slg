caa3b7ab7ebab71c280b658a619e474dd8b43b8d
Slemmie <thorvejeneriksen@gmail.com>
2022-04-05 05:52:48 +0200

    create tmp_dir_manager files

---
+++ slg/src/tmp_dir_manager.c
@@ -0,0 +1,10 @@
+// implements tmp_dir_manager.h
+
+#include "tmp_dir_manager.h"
+
+#include <stdlib.h>
+
+void clean_exit(int code) {
+	// for now just exit
+	exit(code);
+}
---
+++ slg/src/tmp_dir_manager.h
@@ -0,0 +1,6 @@
+// manages tmp dir and includes utility functions related to it
+
+#pragma once
+
+// clean up tmp dir things, then call exit(code)
+void clean_exit(int code);

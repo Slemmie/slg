c385e8e01f8dfac3d23af39f173d96f7fc81e3fb
Slemmie <thorvejeneriksen@gmail.com>
2022-04-06 01:16:56 +0200

    set up slg.c for compilation launch

--- slg/src/slg.c
+++ slg/src/slg.c
@@ -12,10 +12,30 @@ int main(int argc, char** argv) {
 	create_tmp_dir();
 	
 	// 3. perform compilation
+	for (size_t i = 0; i < argv_info.input_file_count; i++) {
+		if (argv_info.input_file_types[i] == ARGV_FILE_TYPE_SLG) {
+			char* next_file_path = NULL;
+			create_file_of(argv_info.input_file_paths[i], next_file_path);
+			compile(argv_info.input_file_paths[i], next_file_path);
+			free(argv_info.input_file_paths[i]);
+			argv_info.input_file_paths[i] = next_file_path;
+			argv_info.input_file_types[i] = ARGV_FILE_TYPE_ASM;
+		}
+	}
 	
 	// 4. perform assembling
+	if (argv_info.output_file_type == ARGV_FILE_TYPE_ASM) {
+		destruct_argv_info(&argv_info);
+		clean_exit(0);
+	}
+	// ...
 	
 	// 5. perform linking
+	if (argv_info.output_file_type == ARGV_FILE_TYPE_OBJ) {
+		destruct_argv_info(&argv_info);
+		clean_exit(0);
+	}
+	// ...
 	
 	// 6. clean up
 	destruct_argv_info(&argv_info);

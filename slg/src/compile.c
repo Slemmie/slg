db503478454fb18f02df0da68b261515c53d4edd
Slemmie <thorvejeneriksen@gmail.com>
2022-04-06 07:36:09 +0200

    draft compilation pipeline with comments in compile()

--- slg/src/compile.c
+++ slg/src/compile.c
@@ -1,5 +1,13 @@
 // implements compile.h
 
 void compile(const char* input_file_path, const char* output_file_path) {
+	// read input file into buffer
 	
+	// perform preprocessing
+	
+	// tokenize
+	
+	// generate output buffer from token list by individually parsing every function
+	
+	// write buffer to output file
 }

98a8ed1cf473e56f0fc25abf55d490591bfa5d13
Slemmie <thorvejeneriksen@gmail.com>
2022-04-06 09:34:29 +0200

    fill in a few calls in compile()

--- slg/src/compile.c
+++ slg/src/compile.c
@@ -4,12 +4,15 @@
 
 void compile(const char* input_file_path, const char* output_file_path) {
 	// read input file into buffer
+	char* input_buffer = read_file(input_file_path);
 	
 	// perform preprocessing
 	
 	// tokenize
 	
 	// generate output buffer from token list by individually parsing every function
+	char* output_buffer = create_asm_file(/* token list, ... */);
 	
 	// write buffer to output file
+	write_file(output_file_path, output_buffer);
 }
